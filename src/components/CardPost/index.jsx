f30c33a471a6b6e5b614160cf769d20158073fe9
Caio Furegati <caiofu@hotmail.com>
2025-11-05 14:27:59 -0300

    Estilizando card

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,18 +1,20 @@
 import { Avatar } from "../Avatar";
+import styles from './cardPost.module.css';
 import Image from 'next/image';
 export const CardPost = ({post}) => {
     return (
-        <article>
-            <header>
+        <article className={styles.cardPost}>
+            <header >
                 <figure>
                     <Image src={post.cover} width={438} height={133} alt={`Capa do post de titulo: ${post.title}`}/>
                 </figure>
             </header>
-            <section>
+            <section className={styles.text}>
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
             </section>
-            <footer>
+            <footer className={styles.footer}>
+                <button>Leia mais</button>
                 <Avatar name={post.author.name} imageSrc={post.author.avatar} />
             </footer>
         </article>
