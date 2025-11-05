72cc91e1991dc0ff584bfe7020c16a9e3b6b289a
Caio Furegati <caiofu@hotmail.com>
2025-11-05 13:04:51 -0300

    Configuração inicial do projeto

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,11 @@
+import styles from './aside.module.css';
+import logo from './logo.png';
+import Image from 'next/image';
+export const Aside = () => {
+  return (
+    <aside className={styles.aside}>
+        {/* <img src={logo} alt="Logo Code Connect" /> */}
+        <Image src={logo} alt="Logo Code Connect" />
+    </aside>
+  );
+};
